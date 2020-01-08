aee0476f8d707dc69d5b161ccb99314bb129abfc
이수민/P서비스파트/NES <leesm120@nate.com>
2020-01-08 14:36:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,9 @@ else
   echo "!!!$OS install nodejs failed!!!"
 fi
 
-git_TOKEN=3a34fb8e2c9b72b5e850c2f659e33a986dca1744
-curl -sL https://$git_TOKEN@raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/index.js > index.js
-curl -sL https://$git_TOKEN@raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/package.json > package.json
-curl -sL https://$git_TOKEN@raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/package-lock.json > package-lock.json
+curl -sL -H "Authorization: token 0b429980ba9997e62b7772b6aac5f701354d7c0e" https://raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/index.js > index.js
+curl -sL -H "Authorization: token 0b429980ba9997e62b7772b6aac5f701354d7c0e" https://raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/package.json > package.json
+curl -sL -H "Authorization: token 0b429980ba9997e62b7772b6aac5f701354d7c0e" https://raw.githubusercontent.com/nhn/toast.test-tools-public/master/network_traffic_image/tool/package-lock.json > package-lock.json
 
 sudo fallocate -l 209715200 200m
 sudo npm install -g
