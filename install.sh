f4115589075e3e2b81d2a9d87edebf6acc20de5f
이수민/P서비스파트/NES <leesm120@nate.com>
2020-01-08 14:04:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
   sudo yum install -y nodejs
 elif [ $OS == "ubuntu" ] || [ $OS == "debian" ]
 then
-  crul -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+  curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
   sudo apt-get install -y nodejs
 else
   echo "!!!$OS install nodejs failed!!!"
